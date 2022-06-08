d0efd0a98740cf26d6f857f7f090a06c4954890d
kailanajt <kailana.thompson12@gmail.com>
2022-06-08 14:54:23 -0500

    adding bootstrap, javascript

---
+++ js/work.js
@@ -0,0 +1,9 @@
+let myProjects = [
+    {
+    title: 'Tic Tac Toe',
+    deployment: 'https://kailanajt-ttt.netlify.app/',
+    image: '<img src="./assets/ttt.png" ', 
+    github: '',
+    description: '',
+}
+    ]
