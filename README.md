a03b6a46b0dc5f8457ed06b90deb46786d92de23
kailanajt <kailana.thompson12@gmail.com>
2022-06-10 10:15:32 -0500

    added resue

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # kailanajt-portfolio
 credit to: bootstrap for following features
 credit to: Awesome Fonts for incons
+
+getting started: https://kjt-portfolio.netlify.app
--- js/app.js
+++ js/app.js
@@ -2,9 +2,24 @@
 
 
 //cached elems
-
+// const hooray = document.getElementById('btn')
+// const toggleBtn = document.getElementById('toggleBtn')
 
 //event listeners
 
-
 //functions
+
+// function toggleBtn() {
+//     body.className = body.className === "dark" ? "" : "dark"
+//   }
+
+//   function toggleDark() {
+//     if (
+//       window.matchMedia("(prefers-color-scheme:dark)").matches &&
+//       body.className !== "dark"
+//     ) {
+//       toggleDark()
+//     }
+//   }
+
+// toggleDark()
