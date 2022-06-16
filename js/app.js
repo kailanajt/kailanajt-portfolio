2b4f5554add19892da30e124984ab61bdec05b3b
kailanajt <kailana.thompson12@gmail.com>
2022-06-16 07:34:11 -0500

    final javascript implementation

--- js/app.js
+++ js/app.js
@@ -9,19 +9,34 @@ const gitHub = document.getElementById('git')
 const linkedIn = document.getElementById('linked')
 const email = document.getElementById('gMail')
 
-gitHub.classList.add('animate__animated', 'animate__jello')
-linkedIn.classList.add('animate__animated', 'animate__jello')
-email.classList.add('animate__animated', 'animate__jello')
+// gitHub.classList.add('animate__animated', 'animate__jello')
+// linkedIn.classList.add('animate__animated', 'animate__jello')
+// email.classList.add('animate__animated', 'animate__jello')
 
 
 //event listeners
 toggleBtn.addEventListener('click', toggleDark)
-// gitHub.addEventListener('mouseover', () => {
-//   gitHub.classList.add('animate__animated', 'animate__jello')
-//   gitHub.style.setProperty('--animate-duration', '0.5s')
-//   gitHub.classList.remove('animate__animated', 'animate__jello')
+
+gitHub.addEventListener('mouseover', function () {
+  gitHub.classList.add('animate__animated', 'animate__jello', 'animate__repeat-3')
+  // gitHub.style.setProperty('--animate-duration', '1.0')
+  gitHub.classList.remove()
   
-// })
+})
+
+linkedIn.addEventListener('mouseover', function () {
+  linkedIn.classList.add('animate__animated', 'animate__jello', 'animate__repeat-3')
+  // gitHub.style.setProperty('--animate-duration', '1.0')
+  linkedIn.classList.remove()
+  
+})
+
+email.addEventListener('mouseover', function () {
+  email.classList.add('animate__animated', 'animate__jello', 'animate__repeat-3')
+  // gitHub.style.setProperty('--animate-duration', '1.0')
+  email.classList.remove()
+  
+})
 
 //functions
 
