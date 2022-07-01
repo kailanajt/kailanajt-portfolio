f33ae5e7a9de12eb9506dafefec4465df6727745
kailanajt <kailana.thompson12@gmail.com>
2022-06-30 19:31:51 -0500

    add Scribble inn

--- js/projects.js
+++ js/projects.js
@@ -6,6 +6,13 @@ let projects = [
 		image: "/assets/ttt.png",
 		github: 'https://github.com/kailanajt/kailanajt-portfolio',
 		description: 'My first interactive browser app.'
+	},
+	{
+		title: 'The Scribble Inn',
+		deployment: ' https://scribble-inn.herokuapp.com/',
+		image: '/assets/scribble-inn.png',
+		github: 'https://github.com/kailanajt/Scribble-Inn.git',
+		description: 'A safe space to store your ideas.'
 	}
 ]
 
--- js/work.js
+++ js/work.js
@@ -1,15 +1,23 @@
-import {
-	projects
-} from '../js/projects.js'
+import {projects} from '../js/projects.js'
 // console.log(myProjects)
 
 let projectsHTMLArr = projects.map(project => {
 	const html = `
     <h3 id='title'>${project.title}</h3>
+    <div class="project" id="ttt">
     <img src=${project.image} height= 170px width=170px id ='image'>
     <p id='desc'>${project.description}</p>
     <a href='${project.deployment}' id='deploy' class='links'>Play Here!</a>
-    <a href='${project.github}' id='github' class='links'>See how I did it.<a>
+    <a href='${project.github}' id='github' class='links'>Github<a>
+    </div>
+
+    <h3 id='title'>${project.title}</h3>
+    <div class="project" id="scribble-inn">
+    <img src=${project.image} height= 170px width=170px id ='image'>
+    <p id='desc'>${project.description}</p>
+    <a href='${project.deployment}' id='deploy' class='links'>Click Here!</a>
+    <a href='${project.github}' id='github' class='links'>Github<a>
+    </div>
   `
 	return html
 })
