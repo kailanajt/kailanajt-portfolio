7b6b7576aea1afc8db0c95553ab1c96dfe0b683e
kailanajt <kailana.thompson12@gmail.com>
2022-08-12 08:17:58 -0500

    add the grocery store deployment

--- js/projects.js
+++ js/projects.js
@@ -13,6 +13,20 @@ let projects = [
 		image: '/assets/scribble-inn.png',
 		github: 'https://github.com/kailanajt/Scribble-Inn.git',
 		description: 'A safe space to store your ideas.'
+	},
+	{
+		title: 'Code-Ipedia',
+		deployment: 'https://code-ipedia.netlify.app/',
+		image: '/assets/code-ipedia.png',
+		github: 'https://github.com/jsantia85/code-ipedia-front-end',
+		description: 'The following MERN stack application is called Code-Ipedia! Code-Ipedia is an application for saving and sharing functional code.'
+	},
+	{
+		title: 'The Grocery Store',
+		deployment: 'https://thegrocerystore.herokuapp.com/',
+		image: '/assets/the-grocery.png',
+		github: 'https://github.com/kailanajt/thegrocerylist',
+		description: 'Explore my final project at General Assembly.'
 	}
 ]
 
--- js/work.js
+++ js/work.js
@@ -10,14 +10,6 @@ let projectsHTMLArr = projects.map(project => {
     <a href='${project.deployment}' id='deploy' class='links'>Play Here!</a>
     <a href='${project.github}' id='github' class='links'>Github<a>
     </div>
-
-    <h3 id='title'>${project.title}</h3>
-    <div class="project" id="scribble-inn">
-    <img src=${project.image} height= 170px width=170px id ='image'>
-    <p id='desc'>${project.description}</p>
-    <a href='${project.deployment}' id='deploy' class='links'>Click Here!</a>
-    <a href='${project.github}' id='github' class='links'>Github<a>
-    </div>
   `
 	return html
 })
